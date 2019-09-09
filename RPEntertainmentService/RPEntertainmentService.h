95a94c7c72a31c2c3f1969e487dcd9ab318d79e9
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2019-09-08 19:06:55 -0500

    Initial Commit

---
+++ RPEntertainmentService/RPEntertainmentService.h
@@ -0,0 +1,19 @@
+//
+//  RPEntertainmentService.h
+//  RPEntertainmentService
+//
+//  Created by Javier Bolaños on 9/8/19.
+//  Copyright © 2019 gipsyhub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RPEntertainmentService.
+FOUNDATION_EXPORT double RPEntertainmentServiceVersionNumber;
+
+//! Project version string for RPEntertainmentService.
+FOUNDATION_EXPORT const unsigned char RPEntertainmentServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPEntertainmentService/PublicHeader.h>
+
+
